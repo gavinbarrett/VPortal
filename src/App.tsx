31a080a5c22dd8872c75104989c649d9c3865acb
Gavin <gavinbrrtt@gmail.com>
2020-11-06 09:46:51 -0800

    Successfully added video stream to React page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React, {createRef, useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 import './components/styling/App.scss';
 
 const App = ({}) => {
-	return (<div className="hello">"hello"</div>);
+	
+	const constraints = {
+		audio: false,
+		video: true
+	};
+	
+	const handleSuccess = (stream, video) => {
+		//window.URL.createObjectURL(stream);
+		//video.srcObject = stream;
+		console.log(stream);
+		video.srcObject = stream;
+	}
+
+	const handleError = (error) => {
+		console.log(`Error: ${error.message}, ${error.name}`);
+	}
+
+	useEffect(() => {
+		const video = document.getElementById('videoplayer');
+		navigator.mediaDevices.getUserMedia(constraints).then((stream) => handleSuccess(stream, video)).catch(handleError);
+	}, []);
+
+	return (<div className="container">
+		<video id="videoplayer" playsInline={true} autoPlay={true}>
+		</video>
+	</div>);
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
